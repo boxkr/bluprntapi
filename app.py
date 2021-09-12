7b25ef126b5f97e86309c006e6951e1f24596572
boxkr <keeg9087@gmail.com>
2021-09-11 19:07:03 -0500

    first

---
+++ app.py
@@ -0,0 +1,22 @@
+from flask import Flask, jsonify, request, render_template
+from flask_cors import CORS
+
+
+app = Flask(__name__)
+if __name__ == "__main__":
+    app.run(port=5000)
+
+@app.route('/')
+def homepage():
+    return "<h1>Hello Word!</h1>"
+
+@app.route('/data', methods=["GET","POST"])
+def data():
+
+    #incoming data
+
+    print("request:",request)
+    if request.method == "POST":
+        print("getting post request")
+
+    return 'hi'
