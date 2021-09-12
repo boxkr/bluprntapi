5929fbb1a2c5aacc4bad61c157d9b35d45b8af15
boxkr <keeg9087@gmail.com>
2021-09-11 19:36:07 -0500

    testing returns

--- app.py
+++ app.py
@@ -19,6 +19,6 @@ def data():
     if request.method == "POST":
         print("getting post request")
         requestJSON = request.json
-        return jsonify(requestJSON)
+        return "yo whats up"
 
     return "hi"
